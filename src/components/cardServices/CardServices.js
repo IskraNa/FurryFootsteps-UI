da808d052ad38599571c7f1a396702643435d405
Maja Vuevska <maja.vuevska@gmail.com>
2024-05-12 17:11:51 +0200

    Add paging and filtering

--- src/components/cardServices/CardServices.js
+++ src/components/cardServices/CardServices.js
@@ -1,99 +1,130 @@
-import React, { useState } from 'react';
-import servicesData from './serviceData.json';
-import './CardService.css';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "./CardService.css";
+import { Link } from "react-router-dom";
+import getAllPosts from "../../services/postsService/getAllPosts.js";
+import getAllActivityTypes from "../../services/activityType/getAllActivityTypes.js";
 
 function CardService({ service }) {
-    return (
-        <div className="card-service">
-            <Link to={`/details/${service.id}`}>
-                <img src={require(`../../assets/${service.image}`)} alt={service.serviceName} />
-            </Link>
-            <div className="card-details">
-                <h3>{service.serviceName}</h3>
-                <p>Gender: {service.gender} - Age: {service.age}</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="card-service">
+      <Link to={`/details/${service.id}`}>
+        <img
+          //src={require(`../../assets/${service.image}`)}
+          src={require(`../../assets/dog_walking.png`)}
+          alt={service.serviceName}
+        />
+      </Link>
+      <div className="card-details">
+        <h3>{service.activityTypeId} - FIX</h3>
+        <p>Pet Size: {service.petSize}</p>
+        <p>Price: ${service.price}</p>
+      </div>
+    </div>
+  );
 }
 
 function Pagination({ currentPage, totalPages, onPageChange }) {
-    const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
+  const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
 
-    return (
-        <div className="pagination">
-            <button disabled={currentPage === 1} onClick={() => onPageChange(currentPage - 1)}>&#8592;</button>
-            {pageNumbers.map(number => (
-                <button key={number} onClick={() => onPageChange(number)} className={currentPage === number ? 'active' : ''}>
-                    {number}
-                </button>
-            ))}
-            <button disabled={currentPage === totalPages} onClick={() => onPageChange(currentPage + 1)}>&#8594;</button>
-        </div>
-    );
+  return (
+    <div className="pagination">
+      {currentPage > 1 && (
+        <button onClick={() => onPageChange(currentPage - 1)}>&#8592;</button>
+      )}
+      {pageNumbers.map((number) => (
+        <button
+          key={number}
+          onClick={() => onPageChange(number)}
+          className={currentPage === number ? "active" : ""}
+        >
+          {number}
+        </button>
+      ))}
+      {currentPage < totalPages && (
+        <button onClick={() => onPageChange(currentPage + 1)}>&#8594;</button>
+      )}
+    </div>
+  );
 }
-
-
 
 function CardServices() {
-    const [categories, setCategories] = useState({
-        all: true,
-        grooming: false,
-        walking: false,
-        training: false,
-        adoption: false,
-        events: false,
-        veterinary: false
-    });
+  const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [activityTypeId, setActivityTypeId] = useState(null);
+  const [activityTypes, setActivityTypes] = useState([]);
 
-    const [currentPage, setCurrentPage] = useState(1);
-    const servicesPerPage = 6;
+  const refreshActivityTypes = async () => {
+    try {
+      const response = await getAllActivityTypes();
+      setActivityTypes(response);
+    } catch (error) {
+      console.error("Error fetching activity types:", error);
+    }
+  };
 
-    const handleChange = (event) => {
-        const { name, checked } = event.target;
-        setCategories(prevState => ({
-            ...prevState,
-            [name]: checked
-        }));
-    };
+  useEffect(() => {
+    refreshActivityTypes();
+  }, []);
 
-    const filteredServices = servicesData.filter(service => {
-        if (categories.all) return true;
-        return categories[service.category];
-    });
+  const refreshPosts = async (page, activityTypeId) => {
+    try {
+      const response = await getAllPosts(page - 1, activityTypeId);
+      setPosts(response.content);
+      console.log("postovi", response.content);
+      setTotalPages(response.totalPages);
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    }
+  };
 
-    // Pagination logic
-    const indexOfLastService = currentPage * servicesPerPage;
-    const indexOfFirstService = indexOfLastService - servicesPerPage;
-    const currentServices = filteredServices.slice(indexOfFirstService, indexOfLastService);
-    const totalPages = Math.ceil(filteredServices.length / servicesPerPage);
+  useEffect(() => {
+    refreshPosts(currentPage, activityTypeId);
+  }, [currentPage, activityTypeId]);
 
-    const onPageChange = (pageNumber) => {
-        setCurrentPage(pageNumber);
-    };
+  const handleActivityTypeClick = (id) => {
+    setCurrentPage(1);
+    setActivityTypeId(id === activityTypeId ? null : id);
+  };
 
-    return (
-        <div className="App">
-            <div className="filters">
-                <h3>Services:</h3>
-                {Object.entries(categories).map(([category, checked]) => (
-                    <label key={category}>
-                        <input type="checkbox" name={category} checked={checked} onChange={handleChange} />
-                        {category.charAt(0).toUpperCase() + category.slice(1)}
-                    </label>
-                ))}
-            </div>
-            <div className="services-pagination-container">
-                <div className="card-services">
-                    {currentServices.map(service => (
-                        <CardService key={service.id} service={service} />
-                    ))}
-                </div>
-                <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={onPageChange} />
-            </div>
+  return (
+    <div className="App">
+      <div className="filters">
+        <h3>Services:</h3>
+        <p
+          className={!activityTypeId ? "active" : ""}
+          onClick={() => handleActivityTypeClick(null)}
+        >
+          All
+        </p>
+        {activityTypes.map((activityType) => (
+          <p
+            key={activityType.id}
+            className={activityTypeId === activityType.id ? "active" : ""}
+            onClick={() => handleActivityTypeClick(activityType.id)}
+          >
+            {activityType.type}
+          </p>
+        ))}
+      </div>
+      <div className="services-pagination-container">
+        <div className="card-services">
+          {posts.length > 0 ? (
+            posts.map((service) => (
+              <CardService key={service.id} service={service} />
+            ))
+          ) : (
+            <h2 className="card-services-none">Нема постови</h2>
+          )}
         </div>
-    );
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={setCurrentPage}
+        />
+      </div>
+    </div>
+  );
 }
-
 
 export default CardServices;
