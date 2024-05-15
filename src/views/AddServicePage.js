f44f4c0b5fd5554569a681a4692732b82b055529
Elena Shulevska <shulevskaelena@gmail.com>
2024-05-16 00:26:18 +0200

    add date time picker to the form and make successful save call on the backend

--- src/views/AddServicePage.js
+++ src/views/AddServicePage.js
@@ -1,21 +1,22 @@
 import React from "react";
-import addServiceForm from "../components/addServiceForm/addServiceForm";
-import Header from "../components/header/Header";
-import Footer from "../components/footer/Footer";
 import "../components/addServiceForm/addServiceForm.css";
 import AddServiceForm from "../components/addServiceForm/addServiceForm";
 import HeaderSmall from "../components/headerSmall/HeaderSmall";
+import {LocalizationProvider} from "@mui/x-date-pickers";
+import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
 
 function App({ activityTypes, petTypes, userId }) {
   return (
     <div className="app-container">
       <HeaderSmall />
       <div className="content-container">
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
         <AddServiceForm
           activityTypes={activityTypes}
           petTypes={petTypes}
           userId={userId}
         />
+        </LocalizationProvider>
       </div>
     </div>
   );
